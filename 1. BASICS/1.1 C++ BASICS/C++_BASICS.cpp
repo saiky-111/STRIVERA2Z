86b5f8e1355e678dcdb466d231230bba37a494f5
Saikat Dhibar <saikat111dhibar@gmail.com>
2024-06-15 22:50:24 +0530

    initial commit

---
+++ 1. BASICS/1.1 C++ BASICS/C++_BASICS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>//takes more time to compile but it's okk
+#include <iostream> //this library allows us to take inputs and give output. 
+#include <math.h>//mathematical function
+#include <string>// to use string function
+using namespace std;
+int main(){
+    cout<<"Hey Saikat!"<<endl<<"hey saiky!"<<"\n";
+   cout<<"Hey Dhibar!"; //cout is a function that exists under std.
+    int x;
+cin>>x;
+cout<<"Hey Saikat!"<<x;
+//int 
+int x=10;
+// int --> 4 bytes  --> Range is: -2147483648 to 2147483647 // beyond this it cannot store
+     
+    return 0;
+}
